007b7d37fe8d8c7ba21874c043bb6a0ce93d9121
Marta <marta.buchalska.programista@gmail.com>
2022-06-19 20:27:41 +0200

    Initial commit. JavaFX, GSON.

---
+++ src/main/java/pl/martabuchalska/Launcher.java
@@ -0,0 +1,12 @@
+package pl.martabuchalska;
+
+/**
+ * Hello world!
+ *
+ */
+public class Launcher
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
