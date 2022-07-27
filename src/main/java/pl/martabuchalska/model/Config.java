a1b16665fef57e9ca86747cba386a8d69f7c7ac3
Marta <marta.buchalska.programista@gmail.com>
2022-07-28 00:40:37 +0200

    Empty API Key

--- src/main/java/pl/martabuchalska/model/Config.java
+++ src/main/java/pl/martabuchalska/model/Config.java
@@ -3,5 +3,5 @@ package pl.martabuchalska.model;
 public class Config {
 
     // add your API key from http://api.openweathermap.org
-    public final static String API_KEY = "1cf5072fa11cc75eb993dcbf90756477";
+    public final static String API_KEY = "";
 }
